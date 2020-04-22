54f39cf22f3d7890ad92f43e6ad1bc9fe9a9fabf
burakozdemir32 <burakozdemir32@gmail.com>
2020-04-22 12:38:48 +0300

    Remove sample fixtures and tests

--- tests/test_validation.py
+++ tests/test_validation.py
@@ -28,22 +28,6 @@ from estonian_e_invoice.entities import (
 from estonian_e_invoice.validation.exceptions import ValidationError
 
 
-@pytest.fixture
-def response():
-    """Sample pytest fixture.
-
-    See more at: http://doc.pytest.org/en/latest/fixture.html
-    """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
-
-
-def test_content(response):
-    """Sample pytest test function with the pytest fixture as an argument."""
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
-
-
 def test_header_validation():
     # Test date, file_id and version are required
     with pytest.raises(ValidationError) as validation_error:
